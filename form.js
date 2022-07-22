2593f28d8be46024e34c9a273840c2f0c886e350
Alsation57 <71977559+Alsation57@users.noreply.github.com>
2022-07-22 21:46:32 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,51 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDOEKHHBjZO16DYNlqkieEI7ixh7rTHbqc",
+    authDomain: "authentificationapp-ae71b.firebaseapp.com",
+    databaseURL: "https://authentificationapp-ae71b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "authentificationapp-ae71b",
+    storageBucket: "authentificationapp-ae71b.appspot.com",
+    messagingSenderId: "754101945006",
+    appId: "1:754101945006:web:97f81aea78affaba56a33f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth =  firebase.auth();
+
+  //signup function
+  function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+    //
+    promise.catch(e=>alert(e.message));
+    alert("SignUp Successfully");
+  }
+
+  //signIN function
+  function  signIn(){
+    var email = document.getElementById("email");
+    var password  = document.getElementById("password");
+    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+  }
+
+
+  //signOut
+
+  function signOut(){
+    auth.signOut();
+    alert("Signed Out!");
+    window.open("https://lunar-blox.xyz/register", "_self")
+  }
+
+  //active user to homepage
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+      var email = user.email;
+      alert("Active user "+email);
+
+    }else{
+      alert("No Active user Found")
+    }
+  })
