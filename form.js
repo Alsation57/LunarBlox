bc9db4aef458490f671c44ad85e87cb2e66cbb98
Alsation57 <71977559+Alsation57@users.noreply.github.com>
2022-07-25 20:12:56 +0100

    Update form.js

--- form.js
+++ form.js
@@ -28,6 +28,7 @@ const firebaseConfig = {
     var email = document.getElementById("email");
     var password  = document.getElementById("password");
     const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+    window.open("https://lunar-blox.xyz/dashboard", "_self")
   }
 
 
