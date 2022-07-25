b5529450421e3417203d778875972b0ac27963d2
Alsation57 <71977559+Alsation57@users.noreply.github.com>
2022-07-25 23:00:01 +0100

    Update form.js

--- form.js
+++ form.js
@@ -28,7 +28,6 @@ function  signIn(){
   var email = document.getElementById("email");
   var password  = document.getElementById("password");
   const promise = auth.signInWithEmailAndPassword(email.value,password.value);
-  window.open("https://lunar-blox.xyz/dashboard", "_self")
 }
 
 
@@ -45,6 +44,7 @@ firebase.auth().onAuthStateChanged((user)=>{
   if(user){
     var email = user.email;
     emailDisplay.innerText = email;
+    window.open("https://lunar-blox.xyz/dashboard", "_self")
   }else{
       
   }
