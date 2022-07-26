6410a00589fe952f6eae9897c217e01e7185f9cb
Alsation57 <71977559+Alsation57@users.noreply.github.com>
2022-07-26 12:23:08 +0100

    Update form.js

--- form.js
+++ form.js
@@ -21,7 +21,7 @@ function signUp(){
   //
   promise.catch(e=>alert(e.message));
   alert("SignUp Successfully");
-  window.open("https://lunar-blox.xyz/dashboard.html", "_self")
+  window.open("https://lunar-blox.xyz/dashboard", "_self")
 }
 
 //signIN function
@@ -29,7 +29,7 @@ function  signIn(){
   var email = document.getElementById("email");
   var password  = document.getElementById("password");
   const promise = auth.signInWithEmailAndPassword(email.value,password.value);
-  window.open("https://lunar-blox.xyz/dashboard.html", "_self")
+  window.open("https://lunar-blox.xyz/dashboard", "_self")
 }
 
 
